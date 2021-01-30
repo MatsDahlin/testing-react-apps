54b0f1ced82ada108ef26a70d57ed78bdc7e50ea
Mats Dahlin <m.dahlin1@gmail.com>
2021-01-30 22:45:36 +0100

    Exercise 4 - Form Testing - Extra Credit 4 DONE

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,21 +6,23 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import faker from 'faker'
 import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-const buildLoginForm = overrides => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const loginFormBuilder = build('LoginForm', {
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm({password: 'strongerpassword'})
+  const {username, password} = loginFormBuilder({
+    overrides: {password: 'strongerpassword'},
+  })
 
   const usernameField = screen.getByLabelText(/username/i)
   const passwordField = screen.getByLabelText(/password/i)
