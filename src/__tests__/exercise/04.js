574e8ad2478d31405ba63ad7aa954519365ffed5
Mats Dahlin <m.dahlin1@gmail.com>
2021-01-30 22:25:10 +0100

    Exercise 4 - Form Testing - Extra Credit 2 DONE

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,15 +4,22 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import faker from 'faker'
 import Login from '../../components/login'
+
+const buildLoginForm = () => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const username = 'Mats'
-  const password = 'bananahammock'
+  const {username, password} = buildLoginForm()
 
   const usernameField = screen.getByLabelText(/username/i)
   const passwordField = screen.getByLabelText(/password/i)
