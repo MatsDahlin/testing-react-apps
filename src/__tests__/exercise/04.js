459db264ba2cdc8e1ece0428b32e23c85df8c2c1
Mats Dahlin <m.dahlin1@gmail.com>
2021-01-30 22:19:50 +0100

    Exercise 4 - Form Testing - Extra Credit 1  DONE

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,9 +7,7 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', () => {
-  let submittedData
-
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
@@ -23,7 +21,7 @@ test('submitting the form calls onSubmit with username and password', () => {
   userEvent.type(passwordField, password)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  expect(submittedData).toEqual({username, password})
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
 })
 
 /*
